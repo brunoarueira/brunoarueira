3c2a3334337eeebe0e6d0390cc1b601c00f78c39
Bruno Arueira <contato@brunoarueira.com>
2021-02-19 19:40:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Hi 👋
+
+My name is Bruno, welcome to my self readme ;)
+
+[![](https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/bruno_arueira)
+[![](https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/brunoarueira)
+
+[➡️ Check out my website](https://www.brunoarueira.com)
+
+## Latest blog posts
+<!-- blog starts -->
+<!-- blog ends -->
+
+More on [https://brunoarueira.com/blog](https://brunoarueira.com/blog)
---
+++ build_readme.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+
+  gem 'rss'
+end
+
+require 'open-uri'
+
+FEED_URL = 'https://www.brunoarueira.com/feed.xml'
+README_FILE = 'README.md'
+LATEST_BLOG_POSTS_REGEX = /^<!-- blog starts -->.*<!-- blog ends -->$/im.freeze
+
+def blog_entries
+  entries = []
+
+  URI.open(FEED_URL) do |rss|
+    feed = RSS::Parser.parse(rss)
+
+    feed.items.each do |item|
+      entries << ["[#{item.title}](#{item.link})"]
+    end
+  end
+
+  entries
+end
+
+latest_blog_posts = <<~REPLACE
+  <!-- blog starts -->
+  #{blog_entries.join("\n")}
+  <!-- blog ends -->
+REPLACE
+
+readme_content = File.read(README_FILE)
+
+readme_content.gsub!(LATEST_BLOG_POSTS_REGEX, latest_blog_posts)
+
+File.open(README_FILE, 'w') do |f|
+  f.write(readme_content)
+end
